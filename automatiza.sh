3f0f4c59bca3a590d1c1da3d635dc2bbc9d062ea
matheusf30 <mfsouza90@gmail.com>
2024-10-28 22:17:17 -0300

    Organizando roteiros para automatização.

--- automatiza.sh
+++ automatiza.sh
@@ -9,13 +9,12 @@ E executar da seguinte forma [./{script}.sh]
 # Pré-Processamento dos Focos de _Aedes_sp. dos Municípios Catarinenses
 """
 Dados brutos disponibilizados pela Diretoria de Vigilância Epidemiológica de Santa Catarina (DIVE/SC).
-Inicialmente disponíveis como tabelas dinâmicas, separadas em anos (recentemente em semestres, por conta do volume de dados).
-Ao final da sequência de execução, deve-se obter uma tabela única em formato pivot, onde:
--- Colunas são todos os municípios com registros de Focos de _Aedes_ sp.;
--- Linhas são as semanas epidemiológicas de toda a série histórica de registro (iniciando em 2012).
+Baixando diretamente da plataforma online TabNet/Sinan.
 """
-python une_focos_dive.py
-python focos_timespace.py
+. get_dengue.sh
+python une_casos_tabnet.py
+python extrai_clima.py
+python extrai_gfs.py
 python focos_pivot.py
 
 # Pré-Processamento dos Casos de Dengue dos Municípios Catarinenses
@@ -26,4 +25,5 @@ Ao final da sequência de execução, deve-se obter uma tabela única em formato
 -- Colunas são todos os municípios com registros de Focos de _Aedes_ sp.;
 -- Linhas são as semanas epidemiológicas de toda a série histórica de registro (iniciando em 2012).
 """
-#python une_casos_sinan.py # Pensar no próximo ano
+#python une_casos_sinan.py # Pensar no próximo ano."
+
