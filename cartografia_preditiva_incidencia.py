375e29c46d204c3d018abba2597e6c53e1d3823a
matheusf30 <mfsouza90@gmail.com>
2025-11-28 12:10:15 -0300

    Roteiros com padronização de figsize, fontsize, espaçamentos, transparência, legendas e afins.

--- cartografia_preditiva_incidencia.py
+++ cartografia_preditiva_incidencia.py
@@ -66,7 +66,7 @@ _MES_ATUAL = _AGORA.strftime("%m")
 _DIA_ATUAL = _AGORA.strftime("%d")
 _ANO_MES = f"{_ANO_ATUAL}{_MES_ATUAL}"
 _ANO_MES_DIA = f"{_ANO_ATUAL}{_MES_ATUAL}{_DIA_ATUAL}"
-_ANO_MES_DIA = "20251124" #Everton - Excluir
+#_ANO_MES_DIA = "20251124" #Everton - Excluir
 _ONTEM = datetime.today() - timedelta(days = 1)
 _ANO_ONTEM = str(_ONTEM.year)
 _MES_ONTEM = _ONTEM.strftime("%m")
